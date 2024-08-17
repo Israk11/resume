a287c0840b0126fa557f10fdba41feca1e38afa8
Israk11 <israkkhanpathan@gmail.com>
2024-08-17 20:56:19 +0530

    created header and footer component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PersonalInfoComponent } from './features/personal-info/personal-info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/personal-info', pathMatch: 'full' },
+  { path: 'personal-info', component: PersonalInfoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { PersonalInfoComponent } from './features/personal-info/personal-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PersonalInfoComponent
   ],
   imports: [
     BrowserModule,
